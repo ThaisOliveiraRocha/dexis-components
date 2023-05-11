f4852285543b7630a55177cfe354611b2f69dea8
Guilherme Teixeira <guilherme.augusto23@hotmail.com>
2023-05-11 09:59:53 -0300

    Exporting SelectWithIcon

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import FilledButton from "./atoms/FilledButton";
 import ChipsWithIcon from "./atoms/ChipsWithIcon";
 import ButtonWithIcon from "./atoms/ButtonWithIcon";
+import SelectWithIcon from "./atoms/SelectWithIcon";
 
-export { FilledButton, ChipsWithIcon, ButtonWithIcon };
+export { FilledButton, ChipsWithIcon, ButtonWithIcon, SelectWithIcon };
