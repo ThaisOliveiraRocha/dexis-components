49a5f9abc688eacfcd31ea7583b42830fb47f0d5
Thais Rocha <thais.rocha@ciandt.com>
2023-05-15 16:57:38 -0300

    feat: adjust jest dependencies

--- jest.config.ts
+++ jest.config.ts
@@ -4,11 +4,16 @@ import { defaults } from 'jest-config';
 // Sync object
 const config: Config.InitialOptions = {
   verbose: true,
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'js', 'ts', 'tsx'],
-  testEnvironment: 'jsdom',
+  moduleFileExtensions: [...defaults.moduleFileExtensions, "js", "ts", "tsx"],
+  testEnvironment: "jsdom",
   moduleNameMapper: {
-    '\\.(css|sass)$': 'identity-obj-proxy',
+    "\\.(css|sass)$": "identity-obj-proxy",
   },
+  transformIgnorePatterns: [
+    "<rootDir>/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+    "<rootDir>/lib/.*\\.d\\.ts$",
+  ],
 };
 
 export default config;
